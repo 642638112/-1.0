e5dc4e5c86e46dab094a9fe5f76468c8d76dc33e
hylzml <642638112@qq.com>
2016-06-01 00:03:38 +0800

    引入框架

---
+++ EarlySleep/app/src/main/java/com/earlysleep/Activity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.earlysleep.Activity;
+
+
+import android.os.Bundle;
+
+import com.earlysleep.R;
+import com.musketeer.baselibrary.Activity.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    public void setContentView(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
