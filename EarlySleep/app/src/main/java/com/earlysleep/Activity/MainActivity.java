f2b17d926b88615bf7b73800ceddcfe67afc0164
hylzml <642638112@qq.com>
2016-06-01 18:31:07 +0800

    新增加了slidingmenu

--- EarlySleep/app/src/main/java/com/earlysleep/Activity/MainActivity.java
+++ EarlySleep/app/src/main/java/com/earlysleep/Activity/MainActivity.java
@@ -2,30 +2,44 @@ package com.earlysleep.Activity;
 
 
 import android.os.Bundle;
+import android.view.Window;
 
+import com.earlysleep.Fragment.MenuLeftFragment;
 import com.earlysleep.R;
-import com.musketeer.baselibrary.Activity.BaseActivity;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends BaseActivity {
-
-
+public class MainActivity extends SlidingFragmentActivity{
     @Override
-    public void setContentView(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        intLeftMenu();
+
+        setBehindContentView(R.layout.left_menu_frame);
     }
 
-    @Override
-    public void initView() {
+
+    private void intLeftMenu() {
+        android.support.v4.app.Fragment leftMenuFragment = new MenuLeftFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.id_left_menu_frame, leftMenuFragment).commit();
+        SlidingMenu menu = getSlidingMenu();
+        menu.setMode(SlidingMenu.LEFT);
+
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+
+        menu.setFadeDegree(0.35f);
+
 
     }
 
-    @Override
-    public void initEvent() {
 
-    }
-
-    @Override
-    public void initData() {
-
-    }
 }
